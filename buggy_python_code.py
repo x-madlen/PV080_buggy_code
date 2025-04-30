435777f4478ed37b5b45293ac33b2f9fbed5d50c
unknown <xmadlen@ad.fi.muni.cz>
2025-04-30 18:51:02 +0200

    possible Code injection fix

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -26,6 +26,7 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     """ Import the requested version (2 or 3) of urllib"""
+    assert eval not in urllib_version and exec not in urllib_version
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
 
